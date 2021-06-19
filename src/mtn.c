474c2597c02406a03e8b5b52528da8546767d3b2
vuduyhoang <vuduyhoang@gmail.com>
2021-06-19 15:51:32 +0700

    remove log

--- src/mtn.c
+++ src/mtn.c
@@ -1047,12 +1047,12 @@ void dump_stream(AVStream * p)
 {
     av_log(NULL, AV_LOG_VERBOSE, "***dump_stream, time_base: %d / %d\n", 
         p->time_base.num, p->time_base.den);
-    av_log(NULL, AV_LOG_VERBOSE, "cur_dts tb?: %"PRId64", start_time tb: %"PRId64", duration tb: %"PRId64", nb_frames: %"PRId64"\n",
-        av_ts2str(p->internal->priv_pts->val), p->start_time, p->duration, p->nb_frames);
+    //av_log(NULL, AV_LOG_VERBOSE, "cur_dts tb?: %"PRId64", start_time tb: %"PRId64", duration tb: %"PRId64", nb_frames: %"PRId64"\n",
+    //   av_ts2str(p->internal->priv_pts->val), p->start_time, p->duration, p->nb_frames);
     // get funny results here. use format_context's.
-    av_log(NULL, AV_LOG_VERBOSE, "cur_dts s?: %.2f, start_time s: %.2f, duration s: %.2f\n",
-        p->internal->priv_pts->val * av_q2d(p->time_base), p->start_time * av_q2d(p->time_base), 
-        p->duration * av_q2d(p->time_base)); // duration can be AV_NOPTS_VALUE 
+    //av_log(NULL, AV_LOG_VERBOSE, "cur_dts s?: %.2f, start_time s: %.2f, duration s: %.2f\n",
+    //    p->internal->priv_pts->val * av_q2d(p->time_base), p->start_time * av_q2d(p->time_base), 
+    //    p->duration * av_q2d(p->time_base)); // duration can be AV_NOPTS_VALUE 
     // field pts in AVStream is for encoding
 }
 
